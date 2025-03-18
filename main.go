33e3e67743a58658bca93dd7d7ad11d671cbad2a
bssmnt <socuting@gmail.com>
2025-03-18 13:09:37 +0000

    added comments

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 )
 
 func main() {
+	// Create a TCP listener on port 42069
 	TCPListener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Printf("error creating TCP listener: %v\n", err)
 		return
 	}
 
+	// Ensure the TCP Listener is closed when the main function exits
 	defer func() {
 		err := TCPListener.Close()
 		if err != nil {
@@ -23,6 +25,7 @@ func main() {
 		}
 	}()
 
+	// Accept incoming connections in a loop
 	for {
 		conn, err := TCPListener.Accept()
 		if err != nil {
@@ -31,6 +34,7 @@ func main() {
 		}
 		fmt.Printf("new connection from %s\n", conn.RemoteAddr())
 
+		// Create a channel that provides lines of text from the connection
 		linesChannel := getLinesChannel(conn)
 		for line := range linesChannel {
 			fmt.Printf("%s\n", line)
@@ -38,11 +42,14 @@ func main() {
 	}
 }
 
+// Reads data from the connection and sends complete lines to a channel
 func getLinesChannel(conn net.Conn) <-chan string {
 	lines := make(chan string)
 
 	go func() {
+		// Ensures the channel gets closed when goroutine exits
 		defer close(lines)
+		// Ensures the connection gets closed when goroutine exits
 		defer func(f net.Conn) {
 			err := f.Close()
 			if err != nil {
@@ -50,10 +57,13 @@ func getLinesChannel(conn net.Conn) <-chan string {
 			}
 		}(conn)
 
+		// Create 8 byte buffer to read data from the connection and variable to accumulate parts
+		// for the current line
 		bytes := make([]byte, 8)
 		currentLine := ""
 
 		for {
+			// Reads data from the connection
 			n, err := conn.Read(bytes)
 			if err != nil {
 				if err == io.EOF {
@@ -63,17 +73,22 @@ func getLinesChannel(conn net.Conn) <-chan string {
 				break
 			}
 
+			// Convert the bytes read into a string and split them by newline
 			stringBytes := string(bytes[:n])
 			parts := strings.Split(stringBytes, "\n")
 
+			// Store each complete part as a line in the channel
 			for i := 0; i < len(parts)-1; i++ {
 				lines <- currentLine + parts[i]
 				currentLine = ""
 			}
 
+			// Store the last part (which may not be a full line)
 			currentLine += parts[len(parts)-1]
 
 		}
+
+		// Send any remaining data to the channel as the last line
 		if currentLine != "" {
 			lines <- currentLine
 		}
